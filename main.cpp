fb2c7eb27c04223627eb79c6133271401744d235
Ktasa <79499317+Ktasa@users.noreply.github.com>
2022-09-08 16:29:40 -0700

    created main file

---
+++ main.cpp
@@ -0,0 +1,35 @@
+
+
+
+
+
+int main()
+{
+    /*input_points();
+    function: prompt and get user input for 3 points and the starting coordinate
+    -set up environement that displays screen and allows mouse interaction
+    -loop checks for inputs*/
+
+    /*Use classes?
+    - keep track of 3 vertices + most recent point
+    - member functions make relevant calculations with this data
+    */
+
+    /*
+    draw_points();
+    - begin drawing loop
+    - get user input to determine when it stops "click anywhere to finish drawing"
+    - or just use a for loop with predetermined amount of points
+
+    points::random_vertex();
+    pick one of the vertices at random
+
+    points::midpoint();
+    calculate midpoint between vertex and last point
+
+    points::draw_point(); draw point with given coordinates
+
+    */
+
+    return 0;
+}
