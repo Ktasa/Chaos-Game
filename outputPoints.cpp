4709d74d2eceb08ea476b4fabca6a21c13e1d512
Bill Tasabia <billtasabia@gmail.com>
2022-09-13 14:22:40 -0700

    update outputPoints.cpp

--- outputPoints.cpp
+++ outputPoints.cpp
@@ -8,19 +8,21 @@ using namespace sf;
 void outputPoints(points& shape, sf::RenderWindow &window)
 {
     srand(time(0));
-
-    //for(int i=0; i<1000; i++)
-    while(!(sf::Mouse::isButtonPressed(sf::Mouse::Left)))
+    //for(int i=0; i<1000; i++) //predetermined output
+    //clicking mouse ends output loop
+    while(window.isOpen() && !(sf::Mouse::isButtonPressed(sf::Mouse::Left)))
     {
-        shape.calcNextPoint();
-        window.clear();
-        shape.drawAllPoints(window);
-        window.display();
-        if(Keyboard::isKeyPressed(Keyboard::Escape))
-        {
-            window.close();
-        }
+            shape.calcNextPoint();
+            window.clear();
+            shape.drawAllPoints(window);
+            window.display();
+            if(Keyboard::isKeyPressed(Keyboard::Escape))
+            {
+                window.close();
+            }
     }
+    //allows closing window after program output finishes
+    //maybe move to main
     while(window.isOpen())
     {
         if(Keyboard::isKeyPressed(Keyboard::Escape))
