b388efbe09478ca78ce544412e44efb25de47353
Bill T <79499317+Ktasa@users.noreply.github.com>
2022-09-13 13:20:12 -0700

    output function

---
+++ outputPoints.cpp
@@ -0,0 +1,27 @@
+#include "points.h"
+#include <SFML/Graphics.hpp>
+#include <ctime>
+using namespace sf;
+//#include <iostream>
+//using namespace std;
+
+void outputPoints(points& shape, sf::RenderWindow &window)
+{
+    srand(time(0));
+
+    //for(int i=0; i<1000; i++)
+    while(!(sf::Mouse::isButtonPressed(sf::Mouse::Left)))
+    {
+        shape.calcNextPoint();
+        window.clear();
+        shape.drawAllPoints(window);
+        window.display();
+    }
+    while(window.isOpen())
+    {
+        if(Keyboard::isKeyPressed(Keyboard::Escape))
+    {
+        window.close();
+    }
+    }
+}
