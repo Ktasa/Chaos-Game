189a803a49335a7cd31be66df84e8879e5c1889f
Bill T <79499317+Ktasa@users.noreply.github.com>
2022-09-13 13:21:46 -0700

    implemented drawAllPoints method and fixed random number generation

--- points.cpp
+++ points.cpp
@@ -1,7 +1,11 @@
 #include "points.h"
 #include <vector>
 #include <cmath>
+#include <cstdlib>
 #include <ctime>
+#include <SFML/Graphics.hpp>
+using namespace std;
+#include <iostream>
 using namespace std;
 
 //this empty constructor might be more useful
@@ -24,11 +28,13 @@ void points::calcNextPoint(){
     //calculate midpoint between last point and random vertex
     //use random number 0-2 as index position of vertex
     //get last point using index size - 1 
-    srand(time(0)); //Creates rng
+    //srand(time(0)); //Creates rng
     point p;
     point vertex;
     p = allPoints[allPoints.size() - 1]; //Last point in the vector
-    vertex = allPoints[rand() % 3]; //Random vertex
+    int randNum = rand() % 3;
+    vertex = allPoints[randNum]; //Random vertex
+    //cout << time(NULL) << randNum << endl;
     //Creating midpoint for x and y
     p.x = (p.x + vertex.x) / 2;
     p.y = (p.y + vertex.y) / 2;
@@ -36,7 +42,14 @@ void points::calcNextPoint(){
     allPoints.push_back(p);
 }
 
-void points::drawAllPoints()
+void points::drawAllPoints(sf::RenderWindow &window)
 {
-    //loop through all vector values to draw points
+    for(int i=0; i<allPoints.size(); i++)
+    {
+        int x = allPoints[i].x;
+        int y = allPoints[i].y;
+        sf::Vertex point(sf::Vector2f(x, y), sf::Color::White);
+        window.draw(&point, 1, sf::Points);
+    }
 }
+
