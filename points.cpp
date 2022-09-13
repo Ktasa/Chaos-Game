5428917cbf73835314a070b8a1a82d33e9bb5051
Bill Tasabia <billtasabia@gmail.com>
2022-09-13 14:48:23 -0700

    updated points.cpp

--- points.cpp
+++ points.cpp
@@ -5,8 +5,8 @@
 #include <ctime>
 #include <SFML/Graphics.hpp>
 using namespace std;
-#include <iostream>
-using namespace std;
+//#include <iostream>
+//using namespace std;
 
 
 
@@ -20,8 +20,7 @@ void points::addPoint(point p)
 void points::calcNextPoint(){
     //calculate midpoint between last point and random vertex
     //use random number 0-2 as index position of vertex
-    //get last point using index size - 1 
-    //srand(time(0)); //Creates rng
+    //seed is set outside function for repetitive use
     point p;
     point vertex;
     p = allPoints[allPoints.size() - 1]; //Last point in the vector
@@ -31,7 +30,6 @@ void points::calcNextPoint(){
     //Creating midpoint for x and y
     p.x = (p.x + vertex.x) / 2;
     p.y = (p.y + vertex.y) / 2;
-    //Use pushback to add p in allPoints
     allPoints.push_back(p);
 }
 
@@ -41,8 +39,13 @@ void points::drawAllPoints(sf::RenderWindow &window)
     {
         int x = allPoints[i].x;
         int y = allPoints[i].y;
-        sf::Vertex point(sf::Vector2f(x, y), sf::Color::White);
-        window.draw(&point, 1, sf::Points);
+        sf::Vertex dot(sf::Vector2f(x, y), sf::Color::White);
+        window.draw(&dot, 1, sf::Points);
+        //possible easier code to draw points:
+        //RectangleShape rect(Vector2f{10,10});
+        //rect.setOrigin(Vector2f{x,y});
+        //window.draw(rect);
+        //rect.setFillColor(sf::Color::White);
     }
 }
 
